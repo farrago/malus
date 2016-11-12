804f1206ca62689a587c313dfa1477671ca2eeb8
farrago <github@richardtaylor.info>
2016-11-12 12:04:55 +0000

    Change to mlab database as mongohq/compose is shutting down sandboxes
    Compose (were mongohq) are killing their free sandbox, so moving the
    db to mlab's sandbox which remains free.

--- heroku.js
+++ heroku.js
@@ -3,10 +3,22 @@ var deployd = require('deployd');
 var server = deployd({
   port: process.env.PORT || 5000,
   env: 'staging',
+  /* Mongohq, now compose
   db: {
     host: 'linus.mongohq.com',
     port: 10030,
     name: 'app11183418',
+    credentials: {
+      username: process.env.MONGODB_USERNAME,
+      password: process.env.MONGODB_PASSWORD
+    }
+  }
+  */
+  /* mlab */
+  db: {
+    host: 'ds151697.mlab.com',
+    port: 51697,
+    name: 'heroku_5ntw7t0t',
     credentials: {
       username: process.env.MONGODB_USERNAME,
       password: process.env.MONGODB_PASSWORD
